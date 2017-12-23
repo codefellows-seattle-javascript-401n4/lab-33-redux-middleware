1a4a10d088e58ea3e6108df9bfadaa62e278a7f8
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-12-23 11:11:40 -0800

    CATEGORY_CREATE tests done

--- src/__test__/reducer/categories.test.js
+++ src/__test__/reducer/categories.test.js
@@ -15,4 +15,26 @@ describe('Category Reducer', () => {
         },
       })).toThrowError('no category name given')
   });
+
+  test('CATEGORY_CREATE should throw error if no budget amount given', () => {
+    expect(() => reducer(undefined, {
+        type: 'CATEGORY_CREATE',
+        payload: {
+          name: 'poop',
+          budget: 0,
+        },
+      })).toThrowError('no budget amount given')
+  });
+
+  test('CATEGORY_CREATE returns updated state with given payload', () => {
+    let newState = reducer(undefined, {
+      type: 'CATEGORY_CREATE',
+      payload: {
+        name: 'food',
+        budget: 69,
+      }
+    })
+    expect(newState[0].name).toBe('food');
+    expect(newState[0].budget).toBe(69);
+  });
 });
