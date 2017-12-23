1ae31facbe7a5a68f30c586f9711406236c08301
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-12-23 09:55:54 -0800

    test

--- src/__test__/component/dashboard.test.js
+++ src/__test__/component/dashboard.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import Dashboard from '../../component/dashboard';
 
 describe('DASHBOARD', () => {
   test('test', () => {
-    // let wrapper = mount(<Dashboard />);
-    console.log('hi');
+    let wrapper = shallow(<Dashboard />);
+    // console.log(wrapper);
   })
 })
