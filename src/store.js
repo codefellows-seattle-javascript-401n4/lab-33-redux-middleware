4f32554da023824b4364050f3fa9e8b3ad6a3dc9
Larry Cessna <larry.cessna@outlook.com>
2018-01-27 11:46:41 -0800

    Middleware and reporter added. Done.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import reducer from './reducers/combine-reducers';
+import reporter from './middleware/reporter.js';
 
-export default () => createStore(reducer);
+export default () => createStore(reducer, applyMiddleware(reporter));
